d2702f45c33439e0c5dad636ab4f19bb31266fca
krishnaku2228 <krishnaku2228@gmail.com>
2022-09-15 20:39:35 +0530

    first commit

--- src/main/java/com/krishna/controller/CustomerController.java
+++ src/main/java/com/krishna/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -73,6 +74,20 @@ public class CustomerController
 			   return new ResponseEntity<>(customerResponse,HttpStatus.NOT_FOUND); 
 	 }
        return new ResponseEntity<>(customerResponse,HttpStatus.BAD_REQUEST); 
+	  }catch(Exception e) {
+		  return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
+	  }
+  }
+  @DeleteMapping(value="/deletecustomer/{accountNo}")
+  public ResponseEntity<String> deleteCustomerByAccountNo(@PathVariable("accountNo") String accountNo) 
+  {
+	  try {
+		  String deleteResponse=null;
+	 if(accountNo!=null) {
+		 deleteResponse=customerService.deleteCustomerByAccountNo(accountNo);
+		 return  new ResponseEntity<>(deleteResponse,HttpStatus.OK);
+	 }
+	    return new ResponseEntity<>(deleteResponse,HttpStatus.BAD_REQUEST);
 	  }catch(Exception e) {
 		  return new ResponseEntity<>(null,HttpStatus.INTERNAL_SERVER_ERROR);
 	  }
